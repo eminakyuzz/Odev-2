34249d4a31f2acfcb5cca087e03db7d4e3fb4473
M Emin Akyüz <akyuzz.eminn@gmail.com>
2022-03-19 15:09:41 +0300

    soru3 commit

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle = CumleAl();
+            string[] bulunanSesliHarfler = SesliHarfKontrol(cumle);
+
+            foreach (var item in bulunanSesliHarfler)
+            {
+                Console.Write(item);
+            }
+            
+        }
+
+        static string CumleAl()
+        {
+            Console.WriteLine("Lütfen Bir Cümle Giriniz :");
+            string cumle = Console.ReadLine() ;
+            return cumle;
+        }
+
+        static string[] SesliHarfKontrol(string cumle)
+        {
+            string[] sesliHarfler ={"a","e","ı","i","o","ö","u","ü"};
+            string[] bulunanSesliHarfler = new string[8] ;
+
+            for (int i = 0; i < sesliHarfler.Length; i++)
+            {
+                bool v = cumle.Contains(sesliHarfler[i]);
+                if (v == true)
+                {
+                    bulunanSesliHarfler[i] = sesliHarfler[i];
+                }
+
+            }
+            return bulunanSesliHarfler;
+        }
+    }
+}
